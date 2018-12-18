e05b8b7c7712374554e54cb96c6f03abfd3e9a49
Ash <scorpio@Noisebox2.0>
2018-12-17 21:21:59 -0500

    Test

---
+++ index.js
@@ -0,0 +1,8 @@
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.login('NTI0NDAwMDg5NTQ1MDQ4MDY0.Dvnhtw.pxtuFVllwO79Sd8gaO8k-SAq_Ao');
